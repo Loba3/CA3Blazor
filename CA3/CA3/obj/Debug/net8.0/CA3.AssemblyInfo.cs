adae52c85a8b247c87c41d4985887ac7fc5373b7
Loba Alarape <x00183150@mytudublin.ie>
2025-12-10 17:30:13 +0000

    Fix errors

--- CA3/CA3/obj/Debug/net8.0/CA3.AssemblyInfo.cs
+++ CA3/CA3/obj/Debug/net8.0/CA3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CA3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02ccaa7d94958f0edb0be89d61eee2c854151d08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f06f60300b20b2584c178cd1eb27c4eeabe68ba1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CA3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CA3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
